2eea56a8462c45eb0279d85e2fb960d3b1e546e5
matgrand <matteo.grandin@gmail.com>
2023-12-12 17:29:24 +0100

    animation working

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from interfaces import Wire, MagField, create_example_path, create_grid
 from symbolic import SymWire, SymMagField
 from fem import FemWire, FemMagField
 from time import time
+from tqdm import tqdm
 
 import numpy as np, matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -10,12 +11,14 @@ from matplotlib.quiver import Quiver
 #import Axes3D for 3d plotting
 from mpl_toolkits.mplot3d import Axes3D
 
+FIGSIZE = (12,8)
+
 #set numpy print options
 np.set_printoptions(precision=2, suppress=True, linewidth=200)
 
 def test_streamplot():
     #streamplot magnetic field
-    fig = plt.figure(figsize=(80,80))  # big figure just to makeit full screen
+    fig = plt.figure(figsize=FIGSIZE)  # big figure just to makeit full screen
     g3, b3 = grid.reshape((ix,iy,iz,3)), mf.B.reshape((ix,iy,iz,3))
     # gp3x, gp3y, gp3z = g3[0,:,:,0], g3[0,:,:,1], g3[0,:,:,2]
     # bp3x, bp3y, bp3z = b3[0,:,:,0], b3[0,:,:,1], b3[0,:,:,2]
@@ -32,7 +35,7 @@ def test_streamplot():
 
 def test_magfield_plot():
     #plot magnetic field and wire
-    fig = plt.figure(figsize=(80,80))  # big figure just to makeit full screen
+    fig = plt.figure(figsize=FIGSIZE)  # big figure just to makeit full screen
     ax = fig.add_subplot(111, projection='3d')
     dec = max(1, ix*iy*iz//4999)
     cmap = np.log(1000*np.clip(mf.normB[::dec], 0, 0.01))
@@ -44,9 +47,9 @@ def test_magfield_plot():
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
-    ax.set_xlim(-3,3)
-    ax.set_ylim(-3,3)
-    ax.set_zlim(-3,3)
+    ax.set_xlim(GRID_LIM)
+    ax.set_ylim(GRID_LIM)
+    ax.set_zlim(GRID_LIM)
     plt.tight_layout()
     plt.show()
 
@@ -54,12 +57,14 @@ def test_magfield_plot():
 def test_magfield_animation():
     pass
 
+GRID_LIM = (-3,3)
+
 if __name__ == '__main__':
-    ix, iy, iz = 20,20,20
-    grid = create_grid((-3,3), (-3,3), (-3,3), n=(ix,iy,iz)) #create a grid
-    wp1 = create_example_path(n=3, r=2.0, z=0.0) #create a wire path
-    wp2 = create_example_path(n=4, r=2.0, z=1.5) #create a wire path
-    wp3 = create_example_path(n=5, r=2.0, z=-1.5) #create a wire path
+    ix, iy, iz = 50,50,50
+    grid = create_grid(GRID_LIM, GRID_LIM, GRID_LIM, n=(ix,iy,iz)) #create a grid
+    wp1 = create_example_path(n=16, r=2.0, z=0.0) #create a wire path
+    wp2 = create_example_path(n=16, r=2.0, z=1.5) #create a wire path
+    wp3 = create_example_path(n=16, r=2.0, z=-1.5) #create a wire path
 
     ## FEM
     w1 = FemWire(wp1, V=50,  seg_len=0.1) #create a wire
@@ -79,32 +84,50 @@ if __name__ == '__main__':
 
     # test_magfield_plot()
 
-    # test_streamplot()
+    # # test_streamplot()
 
     # test_magfield_animation()
-    fig = plt.figure(figsize=(80,80))  # big figure just to makeit full screen
+    NIDXS = 100 #number of idxs to plot
+    STEP_SIZE = 100.0
+    N_ITER = 200 #number of iterations
+    fig = plt.figure(figsize=FIGSIZE)  # big figure just to makeit full screen
     ax = fig.add_subplot(projection='3d')
-    ax.set(xlim=(-3,3), ylim=(-3,3), zlim=(-3,3), xlabel='x', ylabel='y', zlabel='z')
+    ax.set(xlim=GRID_LIM, ylim=GRID_LIM, zlim=GRID_LIM, xlabel='x', ylabel='y', zlabel='z')
     ax.quiver(grid[0,0], grid[0,1], grid[0,2], mf.B[0,0], mf.B[0,1], mf.B[0,2],
                         length=0.4, normalize=True, color=plt.cm.viridis(0), arrow_length_ratio=0.0)
     for w in wires: w.plot(ax, color='r') #plot wires
+    
+    cmap = np.log(1000*np.clip(mf.normB, 0, 0.01))
+
+    curr_idxs = np.random.randint(0, ix*iy*iz, NIDXS)
+    all_idxs = np.zeros((N_ITER, NIDXS), dtype=np.int32)
+    for i in tqdm(range(N_ITER), desc='animating', ncols=80, leave=False):
+        all_idxs[i] = curr_idxs
+        next_pos = grid[curr_idxs] + STEP_SIZE*mf.B[curr_idxs]
+        l = len(next_pos)
+        next_idxs = np.zeros(l, dtype=np.int32)
+        #get element idxs closest to next_pos
+        for j in range(l):
+            next_idxs[j] = np.argmin(np.linalg.norm(grid - next_pos[j], axis=-1))
+        #remove idxs that are the same as curr_idxs
+        next_idxs = next_idxs[next_idxs != curr_idxs]
+        curr_idxs[len(next_idxs):] = np.random.randint(0, ix*iy*iz, NIDXS-len(next_idxs))
+        curr_idxs[:len(next_idxs)] = next_idxs
 
     def update(id):
         #randomly select elements of grid and quiver them
-        ni = 50
-        idx = np.random.randint(0, ix*iy*iz, ni)
-        gridi = grid[idx]
-        Bi = mf.B[idx]
-        cmap = np.log(1000*np.clip(mf.normB, 0, 0.01))
+        gridi = grid[all_idxs[id]]
+        Bi = mf.B[all_idxs[id]]
         ax.clear()
-        ax.set(xlim=(-3,3), ylim=(-3,3), zlim=(-3,3), xlabel='x', ylabel='y', zlabel='z')
+        ax.set(xlim=GRID_LIM, ylim=GRID_LIM, zlim=GRID_LIM, xlabel='x', ylabel='y', zlabel='z')
         ax.quiver(gridi[:,0], gridi[:,1], gridi[:,2], Bi[:,0], Bi[:,1], Bi[:,2],
-                        length=0.4, normalize=True, color=plt.cm.viridis(cmap[idx]), arrow_length_ratio=0.0)
+                        length=0.4, normalize=True, color=plt.cm.inferno(cmap[all_idxs[id]]), arrow_length_ratio=0.0)
         for w in wires: w.plot(ax, color='r') #plot wires
-        print(f'idx: {id}')
         return ax
 
-    ani = animation.FuncAnimation(fig=fig, func=update, frames=10, interval=20, blit=False, repeat=False)
+    ani = animation.FuncAnimation(fig=fig, func=update, frames=N_ITER, interval=100, blit=False, repeat=True)
+    # save animation as gif
+    # ani.save('anim.gif', writer='imagemagick')
     plt.show()
 
 
