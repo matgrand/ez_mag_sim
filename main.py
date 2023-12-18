1d15f875a1cf645078bcd5de4ccd23c7e85b0a87
matgrand <matteo.grandin@gmail.com>
2023-12-18 17:59:29 +0100

    ultra sick animation

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import numpy as np, matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from mpl_toolkits.mplot3d.art3d import Line3DCollection
 
-FIGSIZE = (9,9)
+FIGSIZE = (12,12)
 GRID_LIM = (-4,4)
-ARROW_LEN = 0.4
+ARROW_LEN = 0.3
 
 #set numpy print options
 np.set_printoptions(precision=2, suppress=True, linewidth=200)
@@ -53,10 +53,11 @@ def test_magfield_plot():
     plt.show()
 
 def test_magfield_animation():
-    NIDXS = 100 #number of idxs to plot
-    STEP_SIZE = 0.25 #step size for each iteration
-    N_ITER = 100 #number of iterations
+    NIDXS = 3000 #number of idxs to plot
+    STEP_SIZE = 0.12 #step size for each iteration
+    N_ITER = 2500 #number of iterations
     FPS = 30.0 #frames per second
+    SKIP_FRAMES = 1 #skip frames to reduce animation size
 
     #colors
     import colorsys
@@ -68,63 +69,15 @@ def test_magfield_animation():
 
     fig = plt.figure(figsize=FIGSIZE)  # big figure just to makeit full screen
     ax = fig.add_subplot(projection='3d')
-    ax.set(xlim=GRID_LIM, ylim=GRID_LIM, zlim=GRID_LIM, xlabel='x', ylabel='y', zlabel='z')
-    for w in wires: w.plot(ax, color='r') #plot wires
+    ax.set(xlim=GRID_LIM, ylim=GRID_LIM, zlim=GRID_LIM, xlabel='x', ylabel='y', zlabel='z', aspect='equal')
+    plt.tight_layout()
 
-    #normalized magnetic field B^ = B / |B|
-    Bn = mf.B / mf.normB[:,np.newaxis]
-
-    curr_idxs = np.random.randint(0, ix*iy*iz, NIDXS)
-    all_idxs = np.zeros((N_ITER, NIDXS), dtype=np.int32)
-    for i in tqdm(range(N_ITER), desc='animating', ncols=80, leave=False):
-        all_idxs[i] = curr_idxs
-        next_pos = grid[curr_idxs] + STEP_SIZE*Bn[curr_idxs]
-        l = len(next_pos)
-        next_idxs = np.zeros(l, dtype=np.int32)
-        #get element idxs closest to next_pos
-        for j in range(l): next_idxs[j] = np.argmin(np.linalg.norm(grid - next_pos[j], axis=-1))
-        #remove idxs that are the same as curr_idxs
-        valid_mask = next_idxs != curr_idxs
-        curr_idxs = next_idxs
-        curr_idxs[~valid_mask] = np.random.randint(0, ix*iy*iz, np.sum(~valid_mask))
-
-    def update(id):
-        ax.clear() # clear axis, comment to leave trails
-        #randomly select elements of grid and quiver them
-        gridi, Bi = grid[all_idxs[id]], Bn[all_idxs[id]]
-        ax.set(xlim=GRID_LIM, ylim=GRID_LIM, zlim=GRID_LIM, xlabel='x', ylabel='y', zlabel='z')
-        update_ax(ax, gridi, Bi, linewidths=0.8, colors=rcol)
-        for w in wires: w.plot(ax, color='r') #plot wires
-        return ax
-
-    ani = animation.FuncAnimation(fig=fig, func=update, frames=N_ITER, interval=1000/FPS, blit=False, repeat=True)
-    # save animation as gif
-    # ani.save('anim.gif', writer='imagemagick')
-    plt.show()
-
-def test_magfield_animation2():
-    NIDXS = 400 #number of idxs to plot
-    STEP_SIZE = 0.1 #step size for each iteration
-    N_ITER = 1000 #number of iterations
-    FPS = 30.0 #frames per second
-
-    #colors
-    import colorsys
-    def rainbow_c(idx, n=10): # return a rainbow color
-        c_float = colorsys.hsv_to_rgb(idx/n, 1.0, 1.0)
-        return tuple([int(round(255*x)) for x in c_float])
-    
-    rcol = [rainbow_c(i, NIDXS) for i in range(NIDXS)]
-
-    fig = plt.figure(figsize=FIGSIZE)  # big figure just to makeit full screen
-    ax = fig.add_subplot(projection='3d')
-    ax.set(xlim=GRID_LIM, ylim=GRID_LIM, zlim=GRID_LIM, xlabel='x', ylabel='y', zlabel='z')
     for w in wires: w.plot(ax, color='r') #plot wires
 
     curr_poss = np.random.uniform(GRID_LIM[0], GRID_LIM[1], (NIDXS,3)) #random positions
     all_poss = np.zeros((N_ITER, NIDXS, 3), dtype=np.float32)
     all_mfs = np.zeros((N_ITER, NIDXS, 3), dtype=np.float32)
-    for i in tqdm(range(N_ITER), desc='animating', ncols=80, leave=False):
+    for i in tqdm(range(N_ITER), desc='prep anim', ncols=80, leave=False):
         curr_mf = mf.calc(curr_poss, normalized=True)
         all_poss[i], all_mfs[i] = curr_poss, curr_mf
         next_pos = curr_poss + STEP_SIZE*curr_mf
@@ -134,15 +87,17 @@ def test_magfield_animation2():
         curr_poss[~valid_mask] = np.random.uniform(GRID_LIM[0], GRID_LIM[1], (np.sum(~valid_mask),3)) #random positions
 
     def update(id):
+        ii = (id*SKIP_FRAMES) % N_ITER # trick to skip frames and get a faster animation
         ax.clear() # clear axis, comment to leave trails
-        ax.set(xlim=GRID_LIM, ylim=GRID_LIM, zlim=GRID_LIM, xlabel='x', ylabel='y', zlabel='z')
-        update_ax(ax, all_poss[id], all_mfs[id], linewidths=0.8, colors=rcol)
+        ax.set(xlim=GRID_LIM, ylim=GRID_LIM, zlim=GRID_LIM, xlabel='x', ylabel='y', zlabel='z', aspect='equal', title=f'iteration {id}/{N_ITER}')
+        plt.tight_layout()
+        update_ax(ax, all_poss[ii], all_mfs[ii], linewidths=0.8, colors=rcol)
         for w in wires: w.plot(ax, color='r') #plot wires
+        ax.view_init(elev=30, azim=360*id/N_ITER) #rotate view, comment to be able to rotate view yourself
         return ax
 
     ani = animation.FuncAnimation(fig=fig, func=update, frames=N_ITER, interval=1000/FPS, blit=False, repeat=True)
-    # save animation as gif
-    # ani.save('anim.gif', writer='imagemagick')
+    # ani.save('anim.gif', writer='imagemagick') # save animation as gif
     plt.show()
 
 if __name__ == '__main__':
@@ -155,14 +110,14 @@ if __name__ == '__main__':
     wp1 = create_example_path(n=3, r=2.0, z=-1.0) #create a wire path
     wp2 = create_example_path(n=5, r=2.0, z=1.5) #create a wire path
     # wp3 = create_example_path(n=6, r=2.0, z=-1.5) #create a wire path
-    wp3 = np.array([[[2.5*np.sin(t)],[2.5],[2.5*np.cos(t)]] for t in np.linspace(0,2*np.pi,5+1)]).reshape((-1,3))
-    wp4 = np.array([[[2.5*np.sin(t)],[-2.5],[2.5*np.cos(t)]] for t in np.linspace(0,2*np.pi,7+1)]).reshape((-1,3))
+    wp3 = np.array([[[1.3*np.sin(t)],[3],[1.3*np.cos(t)]] for t in np.linspace(0,2*np.pi,5+1)]).reshape((-1,3))
+    wp4 = np.array([[[3],[1.5*np.sin(t)],[1.5*np.cos(t)]] for t in np.linspace(0,2*np.pi,7+1)]).reshape((-1,3))
 
     ## FEM
     w1 = FemWire(wp1, V=50,  seg_len=0.1) #create a wire
     w2 = FemWire(wp2, V=40,  seg_len=0.1) #create a wire
     w3 = FemWire(wp3, V=-40, seg_len=0.1) #create a wire
-    w4 = FemWire(wp4, V=40, seg_len=0.1) #create a wire
+    w4 = FemWire(wp4, V=-40, seg_len=0.1) #create a wire
     # wires = [w1, w2, w3] 
     wires = [w1, w2, w3, w4]
     # wires = [w1] 
@@ -180,9 +135,8 @@ if __name__ == '__main__':
 
     # test_streamplot()
 
-    # test_magfield_animation()
+    test_magfield_animation()
 
-    test_magfield_animation2()
 
 
 
